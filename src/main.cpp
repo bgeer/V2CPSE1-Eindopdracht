8497e408db7cb3f461cf32e6c3b286368e1c9588
bgeer <bas.vandergeer@student.hu.nl>
2020-10-17 18:59:56 +0200

    copy of hwlib spibus in own project

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "hwlib.hpp"
+#include "ownSpiBus.hpp"
 
 namespace target = hwlib::target;
 
@@ -12,7 +13,7 @@ int main( void ){
    auto sclk = hwlib::invert( _sclk );
    auto mosi = hwlib::target::pin_out{ hwlib::target::pins::d4 };
    
-   auto spi  = hwlib::spi_bus_bit_banged_sclk_mosi_miso{ 
+   auto spi  = hwlib::ownSpiBus{ 
       sclk, mosi, hwlib::pin_in_dummy };
       
    auto dc    = hwlib::target::pin_out{ hwlib::target::pins::d6 };
@@ -25,15 +26,17 @@ int main( void ){
    auto display     = hwlib::st7789_spi_dc_cs_rst( spi, dc, cs, rst );   
    
    for(;;){
+      //used 4 timings, 2 for each because maybe hwlib::cout takes also time.
       auto t1 = hwlib::now_us();
       display.clear( hwlib::red );
       auto t2 = hwlib::now_us();
       auto timeItTook = t2-t1;
-      hwlib::cout<<timeItTook << " Microseconds it took" << hwlib::endl;
-      display.flush();
+      hwlib::cout<<timeItTook << " Microseconds it took to clear" << hwlib::endl;
       auto t3 = hwlib::now_us();
-      timeItTook = t3-t2;
-      hwlib::cout<<timeItTook << " Microseconds it took" << hwlib::endl;
+      display.flush();
+      auto t4 = hwlib::now_us();
+      timeItTook = t4-t3;
+      hwlib::cout<<timeItTook << " Microseconds it took to flush" << hwlib::endl;
 
       display.clear( hwlib::green );
       display.flush();
